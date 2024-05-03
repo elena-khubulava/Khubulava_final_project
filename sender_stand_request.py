715e143e1fc8824aa102d76bd92ef2dc46bb7e14
elena-khubulava <168302442+elena-khubulava@users.noreply.github.com>
2024-05-03 23:16:15 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -8,11 +8,15 @@ def create_order():
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, json=data.body_create)
 
 
-def get_track(track):
+def get_track():
     response = create_order()
     track = response.json()["track"]
     return track
 
+
+
 def get_order_by_track():
-    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH + "track")
+    params = get_track()
+    params = {"t": "number"}
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_PATH, params=params)
 
