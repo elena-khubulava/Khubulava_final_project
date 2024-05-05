5abe8320c5ffe93dd289c0d634e3084e2e8f043a
elena-khubulava <168302442+elena-khubulava@users.noreply.github.com>
2024-05-05 17:35:49 +0300

    Update scooter_api_test.py

--- scooter_api_test.py
+++ scooter_api_test.py
@@ -1,18 +1,14 @@
-
 import sender_stand_request
-
-
-
 
 # Elena Khubulava final project autotest 15-Venus
 def test_succsess_order_create():
     response = sender_stand_request.create_order()
-    track = response.json()["track"]
     assert response.status_code == 201
 
 
 def test_get_order_by_track():
-    response = sender_stand_request.get_order_by_track()
+    track = sender_stand_request.get_track()
+    response = sender_stand_request.get_order_by_track(track)
     assert response.status_code == 200
 
 
